c20e2653ac5d0ca2c7ab8855715ea24fa771e594
Pampamlela <pampamlela@gmail.com>
2026-03-16 16:18:50 +0100

    Authentification OK

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,15 +5,15 @@ import { useAuthStore } from './stores/auth'
 import App from './App.vue'
 import router from './router'
 
-const authStore = useAuthStore()
-
-if (authStore.accessToken) {
-    authStore.fetchUser()
-}
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+const authStore = useAuthStore()
+
+if (authStore.accessToken) {
+    authStore.fetchUser()
+}
